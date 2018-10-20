cb0c3b961f65f2e48bddb27ce395cc2f44c03de2
Leonato Ieiri <leonatoieiri@gmail.com>
2018-10-20 17:32:57 -0300

    Fixed update score error and add reload after match score is updated

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,10 +4,12 @@ class MatchesController < ApplicationController
   # POST /update_score
   def update_score
     match = Match.find(params[:id])
-    match.home_team_score = params["match"]["home_team_score"]
-    match.visitor_team_score = params["match"]["visitor_team_score"]
-    match.status = :done
-    match.save
+    if params["match"]
+      match.home_team_score = params["match"]["home_team_score"]
+      match.visitor_team_score = params["match"]["visitor_team_score"]
+      match.status = :done
+      match.save
+    end
 
     @competition = match.competition
     respond_to do |format|
