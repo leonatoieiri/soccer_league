69bcc466d36439b4b410cc54558ab01c0e5dc396
Leonato Ieiri <leonatoieiri@gmail.com>
2018-10-20 22:13:24 -0300

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+arg = Team.create(name: "ARGENTINA", status: :active)
+aus = Team.create(name: "AUSTRIA", status: :active)
+bel = Team.create(name: "BELGIUM", status: :active)
+bra = Team.create(name: "BRAZIL", status: :active)
+bul = Team.create(name: "BULGARIA", status: :active)
+cam = Team.create(name: "CAMEROON", status: :active)
+
+# Argentina players
+Player.create(number: 12, name: "Pablo", status: :active, team_id: arg.id)
+Player.create(number: 1, name: "Garcia", status: :active, team_id: arg.id)
+Player.create(number: 6, name: "Alerto", status: :active, team_id: arg.id)
+Player.create(number: 3, name: "Costa", status: :active, team_id: arg.id)
+Player.create(number: 14, name: "Morelli", status: :active, team_id: arg.id)
+Player.create(number: 13, name: "Basile", status: :active, team_id: arg.id)
+Player.create(number: 4, name: "Domingo", status: :active, team_id: arg.id)
+
+# Austria players
+Player.create(number: 12, name: "Dreisky", status: :active, team_id: aus.id)
+Player.create(number: 1, name: "L.Denk", status: :active, team_id: aus.id)
+Player.create(number: 5, name: "E.Polts", status: :active, team_id: aus.id)
+Player.create(number: 3, name: "H.Zacher", status: :active, team_id: aus.id)
+Player.create(number: 14, name: "Schroth", status: :active, team_id: aus.id)
+Player.create(number: 13, name: "H.Henze", status: :active, team_id: aus.id)
+Player.create(number: 2, name: "Waldner", status: :active, team_id: aus.id)
+
+# Belgium players
+Player.create(number: 12, name: "Van Hert", status: :active, team_id: bel.id)
+Player.create(number: 1, name: "Van Riet", status: :active, team_id: bel.id)
+Player.create(number: 2, name: "Roche", status: :active, team_id: bel.id)
+Player.create(number: 14, name: "Davries", status: :active, team_id: bel.id)
+Player.create(number: 5, name: "Louis", status: :active, team_id: bel.id)
+Player.create(number: 3, name: "Pesant", status: :active, team_id: bel.id)
+Player.create(number: 13, name: "BoPart", status: :active, team_id: bel.id)
+
+# Brazil players
+Player.create(number: 12, name: "Sousa", status: :active, team_id: bra.id)
+Player.create(number: 1, name: "da Silva", status: :active, team_id: bra.id)
+Player.create(number: 3, name: "Paco", status: :active, team_id: bra.id)
+Player.create(number: 13, name: "Marengez", status: :active, team_id: bra.id)
+Player.create(number: 4, name: "Vincento", status: :active, team_id: bra.id)
+Player.create(number: 6, name: "Ferreira", status: :active, team_id: bra.id)
+Player.create(number: 2, name: "Cicero", status: :active, team_id: bra.id)
+
+# Bulgaria players
+Player.create(number: 12, name: "Saranov", status: :active, team_id: bul.id)
+Player.create(number: 1, name: "Popov", status: :active, team_id: bul.id)
+Player.create(number: 14, name: "Borsev", status: :active, team_id: bul.id)
+Player.create(number: 2, name: "Simeonov", status: :active, team_id: bul.id)
+Player.create(number: 5, name: "Botev", status: :active, team_id: bul.id)
+Player.create(number: 3, name: "Dimitrov", status: :active, team_id: bul.id)
+Player.create(number: 13, name: "Ivanov", status: :active, team_id: bul.id)
+
+# Cameroon players
+Player.create(number: 12, name: "Tchuli", status: :active, team_id: cam.id)
+Player.create(number: 1, name: "Yephett", status: :active, team_id: cam.id)
+Player.create(number: 14, name: "Ombe", status: :active, team_id: cam.id)
+Player.create(number: 4, name: "Takanaki", status: :active, team_id: cam.id)
+Player.create(number: 5, name: "Abombe", status: :active, team_id: cam.id)
+Player.create(number: 2, name: "Kiwi", status: :active, team_id: cam.id)
+Player.create(number: 13, name: "Ngawa", status: :active, team_id: cam.id)
